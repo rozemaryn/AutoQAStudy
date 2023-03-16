3f8e217eae659eb22c079d241a68cbfe876843cb
rozemaryn <animalitos@yandex.ru>
2023-03-16 20:20:19 +0300

    reqres isSorted test

---
+++ src/test/java/api/ReqrestTestSorted.java
@@ -0,0 +1,32 @@
+package api;
+
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.given;
+
+public class ReqrestTestSorted {
+
+    private static final String URL = "https://reqres.in/";
+
+    @Test
+    public void isYearsSorted () {
+        Specifications.installSpecs(Specifications.requestSpec(URL), Specifications.respSpec200());
+        List<ColoursData> colours = given()
+                .when()
+                .get("api/unknown")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", ColoursData.class);
+
+        List <Integer> years = colours.stream().map(ColoursData :: getYear).sorted().collect(Collectors.toList());
+        List <Integer> sortedYears = years.stream().sorted().collect(Collectors.toList());
+
+        Assert.assertEquals(sortedYears, years);
+
+        System.out.println(years);
+        System.out.println(sortedYears);
+    }
+}
