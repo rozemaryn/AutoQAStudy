3aa5279b39e92dc22c2b5cf055c6c155a12d6da9
Maria Kostyanetskaya <animalitos@yandex.ru>
2023-06-05 15:21:32 +0300

    locators practice

--- src/test/java/courseUdemy/Locators.java
+++ src/test/java/courseUdemy/Locators.java
@@ -13,18 +13,21 @@ public class Locators {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\data\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        //driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+        //implicit wait для всех элементов
+        // driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2500));
 
         driver.get("https://rahulshettyacademy.com/loginpagePractise/");
         driver.findElement(By.id("username")).sendKeys("rahulshettyacademy");
         driver.findElement(By.name("password")).sendKeys("incorrectpassword");
         driver.findElement(By.cssSelector("input[type='submit']")).click();
-        //Thread.sleep(2000);
+        // останавливаем поток, потому что элемент появляется не сразу
+        Thread.sleep(2000);
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(3000));
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("div.alert-danger")));
+        // wait для конкретного элемента
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(4000));
+//        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("div.alert-danger")));
 
-        //WebElement element = driver.findElement(By.cssSelector("div.alert-danger"));
+        WebElement element = driver.findElement(By.cssSelector("div.alert-danger"));
         System.out.println(element.getText());
         driver.quit();
     }
---
+++ src/test/java/courseUdemy/LumosityLoginTest.java
@@ -0,0 +1,30 @@
+package courseUdemy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LumosityLoginTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\data\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        driver.get("https://www.lumosity.com/en/");
+        driver.findElement(By.xpath("//a[@data-track-button-press='log_in']")).click();
+        driver.findElement(By.xpath("//input[@placeholder = 'Email']")).sendKeys("animalitos");
+        driver.findElement(By.xpath("//input[@placeholder = 'Email']")).clear();
+        driver.findElement(By.xpath("//input[@placeholder = 'Email']")).sendKeys("animalitos@yandex.ru");
+        driver.findElement(By.xpath("//input[@placeholder = 'Password']")).sendKeys("wrongpassword");
+        driver.findElement(By.name("persistent_login")).click();
+        driver.findElement(By.cssSelector("input.btn-primary")).click();
+
+        driver.findElement(By.xpath("//a[@data-track-link-click = \"forgot_password\"]")).click();
+        driver.
+                findElement(By.xpath("//input[@placeholder=\"Email address\"]")).
+                sendKeys("animalitos@yandex");
+        driver.findElement(By.className("btn-primary")).click();
+    }
+}
